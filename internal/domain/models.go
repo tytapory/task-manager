bb98ba01328ba17c7124dcb5cbb06e758bc6c77e
Vladislav Bezmaternih <vladislav@bezmaternih.com>
2026-08-16 16:37:19 +0500

    Add: models

---
+++ internal/domain/models.go
@@ -0,0 +1,50 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type TeamRole string
+
+const (
+	Owner  TeamRole = "owner"
+	Admin  TeamRole = "admin"
+	Member TeamRole = "member"
+)
+
+type User struct {
+	ID           uuid.UUID
+	Email        string
+	PasswordHash string
+	Name         string
+	CreatedAt    time.Time
+}
+
+type Team struct {
+	ID        uuid.UUID
+	Name      string
+	CreatedBy uuid.UUID
+	CreatedAt time.Time
+}
+
+type TeamMember struct {
+	TeamID uuid.UUID
+	UserID uuid.UUID
+	Role   TeamRole
+}
+
+type Task struct {
+	ID          uuid.UUID
+	TeamID      uuid.UUID
+	Title       string
+	Description string
+	Status      string
+	CreatedBy   uuid.UUID
+	AssigneeID  uuid.UUID
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+	ClosedAt    time.Time
+	Version     uint
+}
